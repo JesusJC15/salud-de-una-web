4b5466868f249d6dd9daa7224f4df1be9f724bde
JesusJC15 <jesus.jauregui-c@mail.escuelaing.edu.co>
2026-03-12 03:23:08 -0500

    bases for the frontend web implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono, Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const interSans = Inter({
+  variable: '--font-inter-sans',
   subsets: ['latin'],
 })
 
@@ -28,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn('font-sans', inter.variable)}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${interSans.variable} antialiased`}
       >
         {children}
       </body>
